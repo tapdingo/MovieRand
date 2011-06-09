0dd2ffd894e75f6da6355bd66da547a9b1dc7abf
Patrick Rehm <tapion@TapsiT400.(none)>
2011-06-09 22:00:18 +0200

    Initial project commit

---
+++ src/com/MovieRand/MovieRand.java
@@ -0,0 +1,13 @@
+package com.MovieRand;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MovieRand extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
